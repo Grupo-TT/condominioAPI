41c9cdb140ac269b99df57d6e8d54e71d00a8719
MichaelGuauque <mcalejo98@gmail.com>
2025-09-29 18:41:12 -0500

    feat: create model implementation

---
+++ src/main/java/com/condominio/persistence/model/RoleEntity.java
@@ -0,0 +1,24 @@
+package com.condominio.persistence.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Entity
+@Table(name = "roles")
+public class RoleEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(name = "role_name")
+    @Enumerated(EnumType.STRING)
+    private RoleEnum roleEnum;
+
+    @ManyToMany(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
+    @JoinTable(name = "role_permission", joinColumns = @JoinColumn(name = "role_id"), inverseJoinColumns = @JoinColumn(name = "permission_id"))
+    private Set<PermissionEntity> permissions = new HashSet<>();
+}
--- src/main/java/com/condominio/persistence/model/UserEntity.java
+++ src/main/java/com/condominio/persistence/model/UserEntity.java
@@ -1,7 +1,6 @@
 package com.condominio.persistence.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -17,4 +16,18 @@ import org.springframework.stereotype.Service;
 public class UserEntity {
     @Id
     private Long id;
+    private String email;
+    private String contrasenia;
+    @Column(name = "is_enable")
+    private boolean isEnabled;
+    @Column(name = "account_no_expired")
+    private boolean accountNoExpired;
+    @Column(name = "account_no_locked")
+    private boolean accountNoLocked;
+    @Column(name = "credential_no_expired")
+    private boolean credentialNoExpired;
+
+    @ManyToMany(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
+    @JoinTable(name="user_roles", joinColumns = @JoinColumn(name="user_id"), inverseJoinColumns = @JoinColumn(name = "role_id"))
+    private Set<RoleEntity> roles = new HashSet<>();
 }
