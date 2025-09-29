6598f116815eb392dc1823bf8a3bdbd9ad5bc689
MichaelGuauque <mcalejo98@gmail.com>
2025-09-29 18:41:55 -0500

    feat: create model implementation

--- src/main/java/com/condominio/persistence/model/RoleEntity.java
+++ src/main/java/com/condominio/persistence/model/RoleEntity.java
@@ -3,6 +3,9 @@ package com.condominio.persistence.model;
 import jakarta.persistence.*;
 import lombok.*;
 
+import java.util.HashSet;
+import java.util.Set;
+
 @Getter
 @Setter
 @AllArgsConstructor
