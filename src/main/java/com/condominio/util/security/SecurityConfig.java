efe05a090c9d3502049e2f1c12c2aa6eff0ff081
VictorDG <victor.gonzalias@correounivalle.edu.co>
2025-10-06 10:10:44 -0500

    feat: implementation of JWT completed

--- src/main/java/com/condominio/util/security/SecurityConfig.java
+++ src/main/java/com/condominio/util/security/SecurityConfig.java
@@ -3,14 +3,19 @@ package com.condominio.util.security;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.annotation.web.configurers.LogoutConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
@@ -19,18 +24,22 @@ import org.springframework.security.web.SecurityFilterChain;
 public class SecurityConfig {
 
     @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity, UserDetailsService userDetailsService)  throws Exception{
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity, UserDetailsService userDetailsService, JwtAuthenticationFilter jwtAuthFilter)  throws Exception{
         return httpSecurity
                 .authorizeHttpRequests(auth -> {
-                    auth.requestMatchers("/login", "/auth/**", "/public/**", "/error").permitAll();
+                    auth.requestMatchers("/login", "/auth/**", "/error").permitAll();
                     auth.anyRequest().authenticated();
+                    // auth.anyRequest().permitAll(); <-//Descomentar si se necesita para probar y comentar linea 28
                 })
                 .cors(cors -> {})
-                .csrf(csrf -> csrf.disable())
+                .csrf(AbstractHttpConfigurer::disable)
                 .sessionManagement(session -> session
                         .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 )
+                .formLogin(form -> Customizer.withDefaults()) // Login Provisional para verificar autenticacion
                 .logout(LogoutConfigurer::permitAll)
+                .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class)
+                //.httpBasic(Customizer.withDefaults()) //configuracion de Autenticacion basica mientras se implementa por completo JWT
                 .userDetailsService(userDetailsService)
                 .build();
     }
@@ -40,16 +49,9 @@ public class SecurityConfig {
         return new BCryptPasswordEncoder();
     }
 
-/*    @Bean
-//    JwtDecoder jwtDecoder() {
-//        return NimbusJwtDecoder.withPublicKey(this.key).build();
-//    }
-//
-//    @Bean
-//    JwtEncoder jwtEncoder() {
-//        JWK jwk = new RSAKey.Builder(this.key).privateKey(this.priv).build();
-//        JWKSource<SecurityContext> jwks = new ImmutableJWKSet<>(new JWKSet(jwk));
-//        return new NimbusJwtEncoder(jwks);
-//    }*/
+    @Bean
+    public AuthenticationManager authManager(AuthenticationConfiguration authConfig) throws Exception{
+        return authConfig.getAuthenticationManager();
+    }
 
 }
