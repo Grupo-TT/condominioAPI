ed1f24199e562aa209ff7e4948f1358d984c6aa6
VictorDGF <vdfg2005@gmail.com>
2025-10-09 14:51:18 -0500

    feat: added constants to format date in token

--- src/main/java/com/condominio/util/constants/AppConstants.java
+++ src/main/java/com/condominio/util/constants/AppConstants.java
@@ -1,8 +1,14 @@
 package com.condominio.util.constants;
 
+import java.time.ZoneId;
+import java.time.format.DateTimeFormatter;
+
 public class AppConstants {
     public static final String PASSWORD_HTML="email/password-temporal";
     public static final String LOGIN_URL="http://localhost:8080";
     public static final String EMAIL_SUBJECT="Tu contraseña temporal - Condominio Flor del Campo";
+    public static final ZoneId ZONE = ZoneId.of("America/Bogota");
+    public static final DateTimeFormatter READABLE_FORMATTER =
+            DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss z");
 
 }
