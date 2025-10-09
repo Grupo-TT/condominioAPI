5ae8abdaeb6425b5906755eba502925109ce8361
Paul Figueroa <paulsfigueroa3@gmail.com>
2025-10-09 13:18:53 -0500

    Remove unused import

--- src/main/java/com/condominio/persistence/repository/ObligacionRepository.java
+++ src/main/java/com/condominio/persistence/repository/ObligacionRepository.java
@@ -1,6 +1,5 @@
 package com.condominio.persistence.repository;
 
-import com.condominio.persistence.model.Casa;
 import com.condominio.persistence.model.Obligacion;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
