4fe3707fee1b258c0ca331864b401d83b786b42f
MichaelGuauque <mcalejo98@gmail.com>
2025-10-27 23:22:19 -0500

    feat: see all recursos comunes

--- src/main/java/com/condominio/service/implementation/RecursoComunService.java
+++ src/main/java/com/condominio/service/implementation/RecursoComunService.java
@@ -1,6 +1,7 @@
 package com.condominio.service.implementation;
 
 import com.condominio.dto.request.RecursoComunDTO;
+import com.condominio.dto.response.RecursoComunPropiDTO;
 import com.condominio.dto.response.SuccessResult;
 import com.condominio.persistence.model.DisponibilidadRecurso;
 import com.condominio.persistence.model.RecursoComun;
@@ -119,5 +120,23 @@ public class RecursoComunService implements IRecursoComunService {
     @Override
     public List<RecursoComun> findByTipoRecurso(TipoRecursoComun tipoRecursoComun) {
         return recursoComunRepository.findByTipoRecursoComun(tipoRecursoComun);
+    }
+
+    @Override
+    public List<RecursoComunPropiDTO> findByDisponibilidad() {
+        List<RecursoComun> recursos = (List<RecursoComun>) recursoComunRepository.findAll();
+        if(recursos.isEmpty()){
+            throw new ApiException("No hay recursos registrados", HttpStatus.BAD_REQUEST);
+        }
+
+        return recursos.stream()
+                .map(recurso -> RecursoComunPropiDTO.builder()
+                        .id(recurso.getId())
+                        .nombre(recurso.getNombre())
+                        .descripcion(recurso.getDescripcion())
+                        .cantidadMaxima(Integer.parseInt(recurso.getCapacidadMaximaUso()))
+                        .disponibilidadRecurso(recurso.getDisponibilidadRecurso())
+                        .build())
+                .toList();
     }
 }
