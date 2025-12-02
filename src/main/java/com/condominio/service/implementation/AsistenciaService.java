814e031fc19fe8785e2760f5628eaa6194bc4d3a
MichaelGuauque <mcalejo98@gmail.com>
2025-12-01 21:24:07 -0500

    feat: asamblea module

--- src/main/java/com/condominio/service/implementation/AsistenciaService.java
+++ src/main/java/com/condominio/service/implementation/AsistenciaService.java
@@ -14,10 +14,8 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
-import java.util.Optional;
 import java.util.stream.Collectors;
 
 @Service
