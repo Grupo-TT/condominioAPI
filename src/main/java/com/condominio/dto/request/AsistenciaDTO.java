bcfe5d794956c17175685e805543c1679c74ed16
MichaelGuauque <mcalejo98@gmail.com>
2025-12-01 21:18:54 -0500

    test: asamblea module

--- src/main/java/com/condominio/dto/request/AsistenciaDTO.java
+++ src/main/java/com/condominio/dto/request/AsistenciaDTO.java
@@ -1,10 +1,12 @@
 package com.condominio.dto.request;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.Getter;
 
 @Data
 @Getter
+@AllArgsConstructor
 public class AsistenciaDTO {
     private int numeroCasa;
     private boolean estado;
