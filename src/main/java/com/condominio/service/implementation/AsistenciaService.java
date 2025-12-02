e0103b91e017e544b42c1c6de8f9bbf602c139aa
MichaelGuauque <mcalejo98@gmail.com>
2025-12-01 20:51:51 -0500

    feat: asamblea module

--- src/main/java/com/condominio/service/implementation/AsistenciaService.java
+++ src/main/java/com/condominio/service/implementation/AsistenciaService.java
@@ -34,31 +34,26 @@ public class AsistenciaService implements IAsistenciaService {
         Asamblea asamblea = asambleaRepository.findById(idAsamblea)
                 .orElseThrow(() -> new ApiException("No existe la asamblea.", HttpStatus.BAD_REQUEST));
 
-        // Extraer números de casa desde el DTO
         List<Integer> numerosDeCasas = asistencias.stream()
                 .map(AsistenciaDTO::getNumeroCasa)
                 .toList();
 
-        // Buscar casas existentes
         List<Casa> casas = casaRepository.findByNumeroCasaIn(numerosDeCasas);
 
-        // Crear un mapa númeroCasa -> DTO para acceder rápido a estado y fecha
         Map<Integer, AsistenciaDTO> asistenciaMap = asistencias.stream()
                 .collect(Collectors.toMap(AsistenciaDTO::getNumeroCasa, dto -> dto));
 
-        // Guardar asistencia para cada casa encontrada
         for (Casa casa : casas) {
             AsistenciaDTO dto = asistenciaMap.get(casa.getNumeroCasa());
 
             Asistencia asistencia = new Asistencia();
             asistencia.setAsamblea(asamblea);
-            asistencia.setCasa(casa);                       // tu setter era setCasas(), debería ser singular
-//            asistencia.setEstado(dto.getEstado());          // ahora viene del DTO
-//            asistencia.setFecha(dto.getFecha());            // también desde el DTO
+            asistencia.setCasa(casa);
+            asistencia.setEstado(dto.isEstado());
+            asistencia.setFecha(asamblea.getFecha());
 
             asistenciaRepository.save(asistencia);
         }
-
         return new SuccessResult<>("Asistencias registradas correctamente.", null);
     }
 }
