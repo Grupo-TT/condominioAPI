f4039136fef4484d7bb13e7ee9d10c4238a43e86
VictorDGF <vdfg2005@gmail.com>
2025-12-09 14:00:14 -0500

    feature: role permissions

--- src/main/java/com/condominio/controller/DashboardPropiController.java
+++ src/main/java/com/condominio/controller/DashboardPropiController.java
@@ -6,6 +6,7 @@ import com.condominio.dto.response.SolicitudPropiDTO;
 import com.condominio.dto.response.SuccessResult;
 import com.condominio.service.interfaces.IDashboardPropiService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -19,16 +20,19 @@ public class DashboardPropiController {
     private final IDashboardPropiService service;
 
     @GetMapping("/info")
+    @PreAuthorize("hasAnyRole('PROPIETARIO', 'ARRENDATARIO')")
     public SuccessResult<InfoCasaPropiDTO> getBasicInfo() {
         return service.getPropiBasicInfo();
     }
 
     @GetMapping("/account-status")
+    @PreAuthorize("hasAnyRole('PROPIETARIO', 'ARRENDATARIO')")
     public SuccessResult<AccountStatusDTO> getAccountStatus() {
         return service.getAccountStatus();
     }
 
     @GetMapping("/solicitudes")
+    @PreAuthorize("hasAnyRole('PROPIETARIO', 'ARRENDATARIO')")
     public SuccessResult<List<SolicitudPropiDTO>> getSolicitudes() {
         return service.getSolicitudesPropietario();
     }
